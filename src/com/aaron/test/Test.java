07638a1aec230fba9556f982e2b5701bc328a529
AaronACER <qikunlele@outlook.com>
2022-10-23 16:49:44 +0800

    hot fix first commit

--- src/com/aaron/test/Test.java
+++ src/com/aaron/test/Test.java
@@ -6,6 +6,7 @@ package com.aaron.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello git4");
         //test
     }
 }
